19bcb2b9b3c9582cefc351e7b5f127a35d92019a
clee <clee@tuta.io>
2021-09-12 16:29:51 -0700

    find nash equilibrium on 2x2 matrix

--- find_nash.py
+++ find_nash.py
@@ -3,26 +3,28 @@ import nashpy as nash
 import numpy as np
 import random
 
-def find_best_response(A,B):
+def find_best_response(A,B, players_response, players_response_index):
     
     index = []
     scores = []
     #print('row_player\r\n',A)
     #print('column_player\r\n',B)
-    i = 0
     max_utility = 0
     for a, b in zip(A,B):
-        for j in range(len(a)):
-            #print('current_j',j)
-            print(a[j],b[j])
-            current_utility = a[j]
+        for i in range(len(a)):
+            #print('current_j',i)
+            #print(a[i],b[i])
+            
+            if players_response == a[i] and players_response_index == i:
+                #print('players_response',players_response, players_response_index)
+                continue
+            current_utility = a[i]
             #print('current_utility',current_utility)
             if current_utility > max_utility:
                 max_utility = current_utility
-                index = np.array([i, j])
-                #print('index',i,j)
-                scores = np.array([a[j], b[j]])
-        i += 1
+                index = i
+                #print('index',i,i)
+                scores = np.array([a[i], b[i]])
     return index, scores, max_utility
 
 # battle of the sexes
@@ -37,5 +39,20 @@ B = np.array([[3, 5], [0, 1]])
 # A = np.array([[3, 0], [5, 1]])
 # B = np.array([[3, 5], [0, 1]])
 
-index, scores, max_utility = find_best_response(A,B)
+index, scores, max_utility = find_best_response(A,B, -1000, -1000)
+#print(index, scores, max_utility)
+
+index = []
+scores = []
+#print('row_player\r\n',A)
+#print('column_player\r\n',B)
+max_utility = 0
+for a, b in zip(A,B):
+    for i in range(len(a)):
+        index, scores, max_utility = find_best_response(A,B, a[i], i)
+        if max_utility >= a[i]:
+            max_utility = a[i]
+            index = i
+            scores = np.array([a[i], max_utility])
+
 print(index, scores, max_utility)
