c004ee3627784283d853ec1c334e0970ab8f6175
clee <clee@tuta.io>
2021-09-17 06:53:45 -0700

    cleanup labels

--- find_nash.py
+++ find_nash.py
@@ -23,8 +23,7 @@ def generate_payout_grid(file):
 def compute_pure_strategies(payout_grid):
     equilibriums = pure_strategy_solutions(payout_grid)
     for s in equilibriums:
-        print("Player 1 plays", s[P1], "and Player 2 plays", s[P2])
-        print("Player 1 score", payout_grid[P2][s[P1]][P1], "and Player 2 score", payout_grid[s[P2]][P2][P2])
+        print(s)
     if len(equilibriums) == 0:
         print("No pure strategies")
 
@@ -36,22 +35,24 @@ def pure_strategy_solutions(payout_grid):
     for c in range(col_num):
         max_payout = max([payout_grid[r][c][P1] for r in range(row_num)])
         for r in range(row_num):
+            #print(payout_grid[r][c][P1])
             if payout_grid[r][c][P1] == max_payout:
-                best_payouts[(r, c)] = (r, c)
+                best_payouts[(r, c)] = max_payout
 
     best_payout_labels = []
     # row then column
     for r in range(row_num):
         max_payout = max([payout_grid[r][c][P2] for c in range(col_num)])
         for c in range(col_num):
+            #print(payout_grid[r][c][P2])
             if payout_grid[r][c][P2] == max_payout:
                 if (r, c) in best_payouts:
-                    best_payout_labels.append(best_payouts[(r, c)])
+                    best_payout_labels.append({(r, c),(best_payouts[(r, c)])})
     return best_payout_labels
 
-#fileName = "games/prisonersDelimma.txt"
+fileName = "games/prisonersDelimma.txt"
 #fileName = "games/PigsGame.txt"
-fileName = "games/HawkDoveGame.txt"
+#fileName = "games/HawkDoveGame.txt"
 file = open(fileName, "r")
 payout_grid = generate_payout_grid(file)
 row_labels = generate_labels(len(payout_grid))
